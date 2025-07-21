fdcd925e3b5adb883c7b07e0baba2f865ae50720
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-07-21 09:28:08 +0200

    add docts terms of use reference

---
+++ Sources/Coenttb Legal Documents/Clause.documentsTermsOfUse.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  coenttb
+//
+//  Created by Coen ten Thije Boonkkamp on 21/07/2025.
+//
+
+import Foundation
+
+//coenttb Docs Terms of Use
+//
+//In addition to the Terms of Use and Privacy Policy applicable to the coenttb site generally (available at cooleygo.com/terms-of-use and cooleygo.com/privacy-policy, respectively), which are incorporated by reference to these terms, the following terms apply to your use of this coenttb document generation system and your agreement to these terms is required before you are permitted to use the system.
+//
+//The coenttb Docs (as defined below) are provided for your reference only and have been developed for companies incorporated in Delaware with primary offices in California.
+//
+//You acknowledge and agree that the making available of these documents (the “coenttb Docs”) to you by Cooley LLP, Cooley (UK) LLP and Cooley SG LLP1  (collectively, “Cooley”) shall not create any attorney-client or other confidential or special relationship between you and Cooley and does not constitute the provision of legal advice or other professional advice by Cooley. The coenttb Docs are for general informational purposes only and should not be relied upon absent a review of your specific circumstances by a qualified professional. Therefore, you should seek advice from an attorney licensed in the relevant jurisdiction(s), as well as a tax professional, before relying on the coenttb Docs. The questionnaire and documents produced by coenttb Docs may be considered Attorney Advertising under the ethical rules of certain jurisdictions.
+//
+//Additionally, the information provided in the coenttb Docs and the questionnaire does not constitute tax advice. Any discussion of tax matters is not intended or written to be used, and cannot be used, for the purpose of avoiding penalties under the Internal Revenue Code (or equivalent in the relevant jurisdiction) or promoting, marketing or recommending to another party any transaction or matter.
+//
+//You further agree and acknowledge that the coenttb Docs have not been prepared with your specific circumstances in mind, may not be suitable for use in your business, and do not constitute tax, accounting or legal advice. By relying on these coenttb Docs, you assume all risk and liability that may result. You should not use coenttb Docs and you should consult your tax, accounting and/or legal advisors in the event the business you are considering organizing has material existing assets, liabilities or operations, or if any assets or contributions are being made by a founder that would cause the value of the company to be anything other than minimal.
+//
+//Review all documents carefully for accuracy before using them. Depending on which documents you choose to generate, there may be YELLOW HIGHLIGHTED portions and other BRACKETED TEXT requiring your attention.
+//
+//COOLEY PROVIDES THESE TERMS ON AN “AS IS” BASIS, AND SPECIFICALLY DISCLAIM ALL WARRANTIES, TERMS, REPRESENTATIONS AND CONDITIONS WHETHER EXPRESS, IMPLIED, OR STATUTORY, AND INCLUDING ANY WARRANTIES, TERMS, REPRESENTATIONS AND CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE, OR NONINFRINGEMENT. WHILE WE TAKE PRECAUTIONS TO PROTECT THE SECURITY OF YOUR INFORMATION, WE CANNOT GUARANTEE IT.
+//
+//You confirm that you have the authority to share any information that you provide about any entity or other individuals. In the ordinary course of the document generation software powering the coenttb Docs, the software may temporarily store on our servers and locally cache data you enter (including copies of the resulting documents generated).  You further confirm that any information you provide is non-confidential and not covered by the attorney-client or any other privileges. We cannot and do not guarantee the confidentiality or security of any data you provide.
+//
+//These terms and the operations or availability of the coenttb Docs document generator may be changed by Cooley, with or without notice, at its sole discretion.
+//
+//1Cooley SG LLP (UEN/Registration No. T19LL1477F) is a licensed foreign law practice registered in Singapore under the Limited Liability Partnerships Act (Chapter 163A) with limited liability.
+//Do you understand and agree to the above terms of use?*
